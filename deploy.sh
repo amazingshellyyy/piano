577846320190cfdcd54badeab26323997c6c87ee
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-22 22:34:56 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ cd ./noise-maker
 npm run build
 echo ">> frontend deployed successfully"
 git add .
-# git commit -m "create docs and ready for deploy"
-# git push origin master
-# echo "|| client-> https://amazingshellyyy.com/piano-player"
+git commit -m "create docs and ready for deploy"
+git push origin master
+echo "|| client-> https://amazingshellyyy.com/piano-player"
